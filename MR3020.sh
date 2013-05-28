41ef5caf7ba25da2748eaf994052db9f18739734
Matthias Strubel <matthias.strubel@aod-rpg.de>
2013-05-28 20:32:17 +0200

    initial commit

---
+++ MR3020.sh
@@ -0,0 +1,5 @@
+## Configuration for MR3020
+make image PROFILE="TLMR3020" PACKAGES="kmod-usb2 kmod-usb-storage kmod-fs-vfat kmod-nls-cp437 kmod-nls-cp850 
+kmod-nls-iso8859-1 kmod-nls-iso8859-15 kmod-fs-ext4 block-mount kmod-loop losetup kmod-batman-adv wireless-tools 
+kmod-lib-crc16 kmod-nls-utf8 kmod-ip6tables kmod-ipt-nat  kmod-ipv6 
+zlib hostapd-mini iw swap-utils -ppp -ppp-mod-pppoe " 
