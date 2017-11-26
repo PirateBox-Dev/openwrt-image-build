241a209f59adb074321941b44d5642588a92d11a
Matthias Strubel <matthias.strubel@aod-rpg.de>
2017-11-26 13:47:22 +0100

    Fix 2nd arch on ramips-mt7620.mk

--- include/ramips-mt7620.mk
+++ include/ramips-mt7620.mk
@@ -7,11 +7,12 @@ ARCH_BUILDROOT=$(ARCH)_musl-1.1.16
 all: \
 	imagebuilder \
 	GLMT300 \
+	GLMT750 \
 	install_zip
 
 GLMT300: \
 	lede-$(LEDE_VERSION)-$(TARGET)-$(TARGET_TYPE)-gl-mt300a-squashfs-sysupgrade.bin \
 	lede-$(LEDE_VERSION)-$(TARGET)-$(TARGET_TYPE)-gl-mt300n-squashfs-sysupgrade.bin
 
-GLMT70: \
+GLMT750: \
 	lede-$(LEDE_VERSION)-$(TARGET)-$(TARGET_TYPE)-gl-mt750-squashfs-sysupgrade.bin
